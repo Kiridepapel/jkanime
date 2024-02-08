beb0e0f46312d0edc3d50d472ff6712448e5e617
Brian Uceda <monalek0622@hotmail.com>
2024-02-08 18:31:45 -0500

    fix(carousel): remove dependency carousel

--- src/app/pages/jkanime/jkanime.component.ts
+++ src/app/pages/jkanime/jkanime.component.ts
@@ -13,9 +13,6 @@ import { ChapterDataDTO } from '../../models/individual.model';
 // Services
 import { DarkModeService } from '../../services/dark-mode.service';
 import { AnimeService } from '../../services/anime.service';
-// Others
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-jkanime',
@@ -24,7 +21,6 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
     CommonModule,
     AnimeItemComponent,
     ChapterItemComponent,
-    CarouselModule,
   ],
   templateUrl: './jkanime.component.html',
   styleUrl: './jkanime.component.scss',
@@ -38,34 +34,12 @@ export class JKAnimeComponent {
   // Darkmode
   private darkModeSubscription!: Subscription;
   public mode!: DarkMode;
-  // Owl carousel
-  carouselOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    autoWidth: true,
-    autoplayTimeout: 5000,
-    autoplaySpeed: 900,
-    mouseDrag: true,
-    touchDrag: false,
-    pullDrag: false,
-    dots: true,
-    navSpeed: 900,
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 1
-      },
-      740: {
-        items: 1
-      },
-      940: {
-        items: 1
-      }
-    },
-    // nav: true
-  };
+  // Carousel
+  private timerCarousel: any;
+  public positionCarousel = 0;
+  public startPosition = 0;
+  public endPosition = 0;
+  public isDragging = false;
 
   constructor(
     private animeService: AnimeService,
@@ -87,6 +61,7 @@ export class JKAnimeComponent {
           this.homeData = data;
           document.title = 'Ver ánime online';
           this.activeList = this.homeData.animesProgramming;
+          this.startTimerCarousel();
         });
     } finally {
       this.isLoading = false;
@@ -95,6 +70,77 @@ export class JKAnimeComponent {
   
   ngOnDestroy() {
     this.darkModeSubscription.unsubscribe();
+    if (this.timerCarousel) {
+      clearInterval(this.timerCarousel);
+    }
+  }
+
+  // Carousel
+  private startTimerCarousel() {
+    clearInterval(this.timerCarousel); // Detener cualquier intervalo existente
+    this.timerCarousel = setInterval(() => {
+      if (this.homeData && this.homeData.sliderAnimes && this.homeData.sliderAnimes.length > 0) {
+        if (this.positionCarousel === this.homeData.sliderAnimes.length - 1) {
+          this.positionCarousel = 0;
+        } else {
+          this.positionCarousel++;
+        }
+        console.log(this.positionCarousel);
+      }
+    }, 5000);
+  }
+
+  public handleDragStart(event: TouchEvent | MouseEvent) {
+    this.startPosition = event instanceof MouseEvent ? event.clientX : event.touches[0].clientX;
+    this.isDragging = true; // Inicia el arrastre
+    clearInterval(this.timerCarousel); // Detiene el intervalo del carrusel
+  }
+  
+  public handleDragEnd(event: TouchEvent | MouseEvent) {
+    if (!this.isDragging) return; // Ignora si no empezó un arrastre
+    this.endPosition = event instanceof MouseEvent ? event.clientX : event.changedTouches[0].clientX;
+    this.handleSwipeGesture();
+    this.isDragging = false; // Termina el arrastre
+    this.startTimerCarousel(); // Reinicia el intervalo del carrusel
+  }
+  
+  public handleDragLeave(event: MouseEvent) {
+    if (this.isDragging) {
+      this.isDragging = false; // Termina el arrastre si el mouse sale del contenedor
+      this.handleSwipeGesture();
+    }
+  }
+  
+  private handleSwipeGesture() {
+    const threshold = 100; // Define un umbral para considerar un movimiento como un deslizamiento (px)
+    const swipeRight = this.endPosition > this.startPosition + threshold;
+    const swipeLeft = this.endPosition < this.startPosition - threshold;
+  
+    if (swipeRight) {
+      this.prevPosCarousel(); // Mueve a la posición anterior
+    } else if (swipeLeft) {
+      this.nextPosCarousel(); // Mueve a la siguiente posición
+    }
+  }
+
+  public changePosCarousel(index: number) {
+    this.positionCarousel = index;
+  }
+
+  public prevPosCarousel() {
+    if (this.positionCarousel > 0) {
+      this.positionCarousel--;
+    } else {
+      this.positionCarousel = this.homeData.sliderAnimes.length - 1;
+    }
+  }
+
+  public nextPosCarousel() {
+    if (this.positionCarousel < this.homeData.sliderAnimes.length - 1) {
+      this.positionCarousel++;
+    } else {
+      this.positionCarousel = 0;
+    }
   }
 
   public toggleDarkMode(event: Event) {
