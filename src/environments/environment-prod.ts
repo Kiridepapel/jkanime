831d2dde567c42eb19cbdd90d8c840e35b81e0b0
Brian Uceda <monalek22@gmail.com>
2024-07-23 01:18:39 -0500

    feat(dev): update domain

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   PRODUCTION: true,
   ENV_NAME: 'production',
   FRONTEND_URL: 'https://fraxianime.vercel.app/',
-  BACKEND_URL: 'https://fraxianime.monalek.xyz/api/v1/',
+  BACKEND_URL: 'https://fraxianime.globitokuki.site/api/v1/',
 };
